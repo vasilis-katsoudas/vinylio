cac74816764919c4baa590023d676d88af500274
Vasilis Katsoudas <vaskats2002@gmail.com>
2025-04-19 19:26:47 +0300

    Final Version + Deployment configuration

---
+++ FinalProject/staticfiles/home/js/password.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const editBtn = document.getElementById("editBtn");
+    const saveBtn = document.getElementById("saveBtn");
+    const inputs = document.querySelectorAll("#profileForm input");
+    const passwordSection = document.getElementById("passwordSection");
+
+    /** show user attributes and change password section */
+    editBtn.addEventListener("click", function () {
+      inputs.forEach(input => input.removeAttribute("readonly"));
+      passwordSection.classList.remove("d-none");
+      editBtn.classList.add("d-none");
+      saveBtn.classList.remove("d-none");
+    });
+  });
